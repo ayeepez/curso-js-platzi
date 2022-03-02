6c53b80bbde6c0d507d045ebe5f7fac0dba2ec96
ayeepez <ayeepez@gmail.com>
2022-03-02 15:52:35 -0500

    figuras y decuentos

--- README.md
+++ README.md
@@ -3,7 +3,20 @@ Platzi
 
 ## Taller #1 figuras geometricas
 
--Primer paso: definir formulas-
+-Primer paso: definir formulas
+
 -Segundo paso: implementar fórmulas en Js 
+
 -Tercer paso: crear funciones
+
 -Cuarto paso:Integrar Js con HTML 
+
+Taller #2 porcentajes y descuentos 
+
+-Definir las formulas 
+
+-Implementar las fórmulas en JS 
+
+-Crear funciones para seleccionar precios que se elijan
+
+-Integrar HTML con JS
--- figuras-js.js
+++ figuras-js.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+let base= parseInt(prompt("Ingrese la base de la figura"))
+let altura=parseInt(prompt("Ingrese la altura de la figura "))
+
+
+console.log("El area del rectangulo es" + area)
+
+
+
+
+let base= parseInt(prompt("Ingrese la base de la figura"))
+let altura=parseInt(prompt("Ingrese la altura de la figura "))
+let area= base * altura 
+
+
+
+function area(base=0,altura=0) {
+console.log("El area de tu figura es ${area})
+}
+
